42577fee993bb829d8b17a6c44e07143f3aca7a3
koudaihirata <2230051@ecc.ac.jp>
2024-10-31 19:08:56 +0900

    天気のAPIを使用するためのコンポーネント作成

--- src/Pages/List/page.tsx
+++ src/Pages/List/page.tsx
@@ -6,21 +6,35 @@ export default function List() {
     const {
         inputValue,
         tasks,
+        bagItems,
         handleInputChange,
         handleAddTask,
         handleKeyDown,
-        handleRemoveTask
+        handleRemoveTask,
+        handleAddBag
     } = TaskHooks();
     
     return (
-        <section style={{ padding: '24px' }}>
+        <section style={{padding: '24px'}}>
             <h2>リスト</h2>
-            <input type="text" value={inputValue} onChange={handleInputChange} onKeyDown={handleKeyDown}  />
-            <button onClick={handleAddTask}>追加</button>
+            <div style={{display:'flex',gap:'24px'}}>
+                <div style={{border:'1px solid #000',padding: '12px'}}>
+                    <input type="text" value={inputValue} onChange={handleInputChange} onKeyDown={handleKeyDown}  />
+                    <button onClick={handleAddTask}>追加</button>
 
-            {tasks.map((task, index) => (
-                <Task key={index} task={task} onRemove={() => handleRemoveTask(index)} />
-            ))}
+                    {tasks.map((task, index) => (
+                        <Task key={index} task={task} onRemove={() => handleRemoveTask(index)} onAdd={() => handleAddBag(index)} />
+                    ))}
+                </div>
+                <div style={{width:'300px',height:'150px',border:'1px solid #000'}}>
+                    <h4>カバンの中</h4>
+                    <ul>
+                        {bagItems.map((item, index) => (
+                            <li key={index}>{item}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </section>
     );
 }
