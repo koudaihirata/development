8156e3963160a28dd000a64523f153b4f15ca45e
koudaihirata <2230051@ecc.ac.jp>
2024-12-06 04:23:58 +0900

    update:アイコンのコンポーネントのスタイル完了

--- src/Iconbox/iconbox.tsx
+++ src/Iconbox/iconbox.tsx
@@ -1,15 +1,27 @@
+import { useState } from 'react';
 import styles from './styles.module.css'
 
 export default function Iconbox(){
+    const [icons, setIcons] = useState<number[]>([]);
+
+    const addIcon = () => {
+        setIcons([...icons, icons.length]);
+    };
+
     return(
         <>
         <div className={styles.boxWrap}>
             <p className={styles.prepare}>準備中</p>
+            <div className={styles.iconBox}>
+                {icons.map((icons) => (
+                    <div key={icons} className={styles.icon}></div>
+                ))}            
+            </div>
             <div className={styles.rect}>
                 <p className={styles.rest}>残り　個</p>
-                <a href="#">
+                <button onClick={addIcon}>
                     <img className={styles.plus} src={"/img/plus.svg"} alt="プラス" />
-                </a>
+                </button>
             </div>
         </div>
         </>
