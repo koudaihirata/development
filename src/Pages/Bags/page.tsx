3570ddfda52099d9ac78fd2984000728bb3525e0
koudaihirata <2230051@ecc.ac.jp>
2024-12-31 18:25:13 +0900

    update:URLで動的にタイトルを変更

--- src/Pages/Bags/page.tsx
+++ src/Pages/Bags/page.tsx
@@ -1,13 +1,23 @@
-
+import { useParams } from "react-router-dom";
 
 interface Props {
     id: string;
 }  
 
 export default function Bags() {
+    const params = useParams<{id: string}>();
+    const id = params.id;
+
+    let URL = "";
+    if (id === "1") {
+        URL = "手持ちバッグ"; 
+    } else if(id === "2") {
+        URL = "キャリーバッグ";
+    }
+
     return(
         <>
-            <Bag id=""/>
+            <Bag id={URL} />
         </>
     )
 }
@@ -15,7 +25,7 @@ export default function Bags() {
 function Bag(props: Props) {
     return(
         <>
-            <p>bag: {props.id}</p>
+            <p>{props.id}</p>
         </>
     )
 }
