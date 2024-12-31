576bfd28a6efe338bd40c3b998e260cc9ce99ca8
koudaihirata <2230051@ecc.ac.jp>
2024-12-31 22:15:46 +0900

    update:カバン内のスタイル完成

--- src/Pages/Bags/page.tsx
+++ src/Pages/Bags/page.tsx
@@ -1,4 +1,5 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import styles from './styles.module.css';
 
 interface Props {
     id: string;
@@ -25,7 +26,41 @@ export default function Bags() {
 function Bag(props: Props) {
     return(
         <>
-            <p>{props.id}</p>
+            <div className={styles.bagDetailWrap}>
+                <div className={styles.bagDetailTitle}>
+                    <Link to={'/'}>
+                        <img src="/img/ReturnArrow.svg" alt="戻る" />
+                    </Link>
+                    <p className={styles.Title}>{props.id}</p>
+                    <p className={styles.edit}>編集</p>
+                </div>
+                <div className={styles.bagMain}>
+                    <div className={styles.bagMainTitle}>
+                        <div className={styles.IconImg}>
+                            <img src="/img/bagkun.png" alt="アイコンイメージ" />
+                        </div>
+                        <p>お風呂セット</p>
+                    </div>
+                    <div className={styles.bagThing}>
+                        <div>
+                            <p className={styles.ProductName}>シャンプー</p>
+                            <p className={styles.Quantity}>1 個</p>
+                        </div>
+                    </div>
+                    <div className={styles.bagThing}>
+                        <div>
+                            <p className={styles.ProductName}>リンス</p>
+                            <p className={styles.Quantity}>1 個</p>
+                        </div>
+                    </div>
+                    <div className={styles.bagThing}>
+                        <div>
+                            <p className={styles.ProductName}>ボディーソープ</p>
+                            <p className={styles.Quantity}>1 個</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
