e31397f40b531ca2a1113cab203250147031f584
koudaihirata <2230051@ecc.ac.jp>
2025-01-22 01:30:10 +0900

    update:カバンの容量によってリスが喋る言葉を変更

--- src/Pages/PackingPage/page.tsx
+++ src/Pages/PackingPage/page.tsx
@@ -2,7 +2,7 @@ import styles from "./styles.module.css";
 import Schedule from "../../components/Schedule/Schedule";
 import IconBox from "../../components/Iconbox/iconbox";
 import Btn from "../../components/Btn/Btn";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState, selectBagCount } from "../../store/store";
@@ -10,6 +10,7 @@ import { RootState, selectBagCount } from "../../store/store";
 export default function PackingPage() {
     const bag1Ref = useRef<HTMLImageElement>(null);
     const bag2Ref = useRef<HTMLImageElement>(null);
+    const [message, setMessage] = useState<string>("null");
 
     const bag1Count = useSelector((state: RootState) =>
         selectBagCount(state, 1)
@@ -21,13 +22,53 @@ export default function PackingPage() {
     const bag1ForImg = Math.min(bag1Count, 6);
     const bag2ForImg = Math.min(bag2Count, 10);
 
+    useEffect(() => {
+        if (bag1ForImg === 6 && bag2ForImg === 10) {
+            setMessage("bagsFull");
+        } else if (bag1ForImg === 6) {
+            setMessage("bag1Full");
+        } else if (bag2ForImg === 10) {
+            setMessage("bag2Full");
+        } else {
+            setMessage("null");
+        }
+    }, [bag1ForImg, bag2ForImg]);
+
     return (
         <div className={styles.bg}>
             <div className={styles.elementBox}>
                 <div className={styles.navWrap}>
                     <div>
-                        <img src="/img/house.png" alt="リスの家" />
+                        {message === "null" && (
+                            <img src="/img/house.png" alt="リスの家" />
+                        )}
+                        {message !== "null" && (
+                            <img src="/img/houseOut.png" alt="リスの家" />
+                        )}
                     </div>
+                    {message === "takeUmbrella" && (
+                        <div className={styles.squirrelMes}>
+                            <img
+                                src="/img/takeUmbrellaMes.png"
+                                alt="メッセージ"
+                            />
+                        </div>
+                    )}
+                    {message === "bag1Full" && (
+                        <div className={styles.squirrelMes}>
+                            <img src="/img/bag1Full.png" alt="メッセージ" />
+                        </div>
+                    )}
+                    {message === "bag2Full" && (
+                        <div className={styles.squirrelMes}>
+                            <img src="/img/bag2Full.png" alt="メッセージ" />
+                        </div>
+                    )}
+                    {message === "bagsFull" && (
+                        <div className={styles.squirrelMes}>
+                            <img src="/img/bagsFull.png" alt="メッセージ" />
+                        </div>
+                    )}
                     <h2>
                         <img src="/img/friend.png" alt="フレンドアイコン" />
                     </h2>
